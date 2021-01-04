2dbfa5d6d4efccc55617c50f6ab45a44345f1353
Muhammad Faris Adi Prabowo <40176160+mfaris16@users.noreply.github.com>
2021-01-04 13:46:09 +0700

    Add files via upload

---
+++ qibla_modern.py
@@ -0,0 +1,5 @@
+from function import *
+
+# Data Koordinat Berdasarkan Google Map
+# Manggil Fungsi Untuk Lokasi Kita
+lokasi(-6.902469, 106.930283) # Masukkan Koordinat Lintang, Kemudian Bujur Lokasi dalam Desimal
